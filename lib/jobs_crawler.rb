f4d5437d26cfabaca7b1841eb8c7604e0279695a
Pathe <pathe.sene@gmail.com>
2019-04-02 10:09:51 +0000

    Ajout d'un méthode crawl à la racine du module.

--- lib/jobs_crawler.rb
+++ lib/jobs_crawler.rb
@@ -5,4 +5,10 @@ require "jobs_crawler/indexers/senjob"
 require "jobs_crawler/indexers/pages_jaunes_senegal"
 
 module JobsCrawler
+  def self.crawl
+    senjob  = Indexers::Senjob.new.crawl["links"]
+    pjaunes = Indexers::PagesJaunesSenegal.new.crawl["links"]
+
+    senjob + pjaunes
+  end
 end
